4d4627cace218d28e17ab8ddc75fd023a6218c14
Ramyashree2023 <ramyareddyrammu99@gmail.com>
2023-10-25 18:03:12 +0530

    React project

---
+++ project-1/src/assets/App.jsx
@@ -0,0 +1,13 @@
+import Navigation from "../components/Navigation";
+import Herosection from "../components/Hero";
+const App = () => {
+    return (
+    <>
+        <Navigation/>
+        <Herosection/>
+    
+        </>
+    );
+};
+
+export default App;
---
+++ project-1/src/components/Hero.jsx
@@ -0,0 +1,35 @@
+
+import React from 'react'
+
+const Herosection = () => {
+    return (
+    <main className="hero">
+        <div className="hero-content">
+            <h1>YOUR FEET DESERVE THE BEST</h1>
+            <p>YOUR FEET DESERVE THE BEST AND WERE HERE TO HELP YOU WITH OUR
+                SHOES. 
+            </p> 
+       
+        <div className="hero-btn">
+            <button>shop now</button> 
+            <button className="secondary-btn">categary</button>
+            </div>
+
+            <div className="shopping">
+                <p>Also Available on</p>
+                </div>
+
+                <div className="brand-icons">
+                    <img className='brand-icons--image' src="https://1000logos.net/wp-content/uploads/2016/10/Amazon-logo-meaning.jpg"/>
+                    <img className='brand-icons--image' src="https://onlineshoppingdeal.in/wp-content/uploads/2020/11/flipkart.png"/>
+
+                </div>
+                </div>
+        
+                <img className="hero-image" src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvZXN8ZW58MHx8MHx8fDA%3D&w=1000&q=80" />
+    
+    </main>
+    );
+}
+
+export default Herosection;
